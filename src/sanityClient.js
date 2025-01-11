83b385eff2c46ee22fe76a2d4ceef7ac9f1e9b67
Ad123 <kkhakkha204@gmail.com>
2025-01-10 21:03:23 -0800

    2

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 const client = sanityClient({
-    projectId: 'your_project_id', // Thay bằng projectId của bạn
+    projectId: 'sr8hoox0', // Thay bằng projectId của bạn
     dataset: 'production', // Dataset của bạn, thường là 'production'
     useCdn: true, // Dùng CDN để truy vấn dữ liệu nhanh hơn (chỉ cần cho dữ liệu công khai)
 });
