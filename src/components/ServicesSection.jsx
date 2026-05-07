df846c25e16de96cad8a83b8e03e7f98481defc0
schhung1822 <schhung2143@gmail.com>
2026-05-07 09:50:33 +0700

    Correct spelling of 'Woodplus' to 'WoodPlus'

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -39,7 +39,7 @@ const ServicesSection = () => {
                                 <span className="ml-14 text-[30px]">&rarr;</span>
                             </Link>
                             <p className="w-[280px] sm:w-[310px] md:w-[350px] 2xl:w-[450px] mx-auto text-[11px] sm:text-[12px] md:text-[13px] lg:text-[14px] xl:text-[15px] tracking-[0.01rem] font-normal text-[#CECECE] mt-1 lg:mt-4">
-                                Với quy trình thiết kế khoa học và chuyên sâu, Woodplus hiện thực hóa ngôi nhà mơ ước
+                                Với quy trình thiết kế khoa học và chuyên sâu, WoodPlus hiện thực hóa ngôi nhà mơ ước
                                 của bạn. Chúng tôi không chỉ nâng tầm không gian sống mà còn tạo nên sự hài hòa giữa
                                 công năng và thẩm mỹ, mang đậm dấu ấn cá nhân của mỗi gia chủ.
                             </p>
@@ -64,7 +64,7 @@ const ServicesSection = () => {
                                 <span className="ml-14 text-[30px]">&rarr;</span>
                             </Link>
                             <p className="w-[280px] sm:w-[310px] md:w-[350px] 2xl:w-[450px] mx-auto text-[11px] sm:text-[12px] md:text-[13px] lg:text-[14px] xl:text-[15px] tracking-[0.01rem] font-normal text-[#CECECE] mt-1 lg:mt-4">
-                                Woodplus cung cấp dịch vụ thi công trọn gói với đội ngũ chuyên môn cao, tận tâm và trung
+                                WoodPlus cung cấp dịch vụ thi công trọn gói với đội ngũ chuyên môn cao, tận tâm và trung
                                 thực trong từng công trình. Chúng tôi cam kết đồng hành chặt chẽ, đảm bảo tiến độ chuẩn
                                 xác, tối ưu chi phí, công năng hoàn hảo và minh bạch trong mọi giai đoạn thi công.
                             </p>
